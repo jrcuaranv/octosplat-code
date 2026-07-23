78adddf7c589140444fc050f199508d6dfc05db6
jrcuaranv <jrcuaranv@gmail.com>
2026-07-23 13:49:12 -0500

    minor

--- configs/eruvae/slam.py
+++ configs/eruvae/slam.py
@@ -207,7 +207,7 @@ config = dict(
         load_semantics=True, # Whether load semantic information
     ),
     active_mapping=dict(
-        data_mode='colmap_dataset', # ['real_robot', 'gazebo_robot', 'colmap_dataset', 'rosbag_file'] (Adjusts Intrinsics and other data-related parameters based on the dataset being used for active mapping)
+        data_mode='gazebo_robot', # ['real_robot', 'gazebo_robot', 'colmap_dataset', 'rosbag_file'] (Adjusts Intrinsics and other data-related parameters based on the dataset being used for active mapping)
         real_robot=dict(
             output_dir= output_dir,
             apply_depth_median_filter = False,
